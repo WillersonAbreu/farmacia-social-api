1183e03061b34d38ed968c7cd0a7c46e7abe2c1a
WillersonAbreu <willersonabreu@hotmail.com>
2020-10-07 22:18:03 -0300

    JWT Token implementado

--- src/main/java/br/com/farmaciasocialapi/controllers/MedicineDonationController.java
+++ src/main/java/br/com/farmaciasocialapi/controllers/MedicineDonationController.java
@@ -1,7 +1,6 @@
 package br.com.farmaciasocialapi.controllers;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
--- src/main/java/br/com/farmaciasocialapi/controllers/UserController.java
+++ src/main/java/br/com/farmaciasocialapi/controllers/UserController.java
@@ -1,6 +1,5 @@
 package br.com.farmaciasocialapi.controllers;
 
-import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,17 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.farmaciasocialapi.models.UserModel;
-import br.com.farmaciasocialapi.repository.UserRepository;
 import br.com.farmaciasocialapi.resources.Response;
 import br.com.farmaciasocialapi.service.UserService;
 
