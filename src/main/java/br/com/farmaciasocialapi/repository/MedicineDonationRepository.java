6bf3e89b7fca77b4fc5f070d487d9585eefcbd80
David Kalil Braga <dmmd10@hotmail.com>
2020-11-18 21:05:26 -0300

    ajuste cards na home e bugfix

--- src/main/java/br/com/farmaciasocialapi/repository/MedicineDonationRepository.java
+++ src/main/java/br/com/farmaciasocialapi/repository/MedicineDonationRepository.java
@@ -2,6 +2,9 @@ package br.com.farmaciasocialapi.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 
 import br.com.farmaciasocialapi.models.MedicineDonationModel;
@@ -9,8 +12,12 @@ import br.com.farmaciasocialapi.resources.BaseRepository;
 
 public interface MedicineDonationRepository extends BaseRepository<MedicineDonationModel> {
 
-	@Query("SELECT d FROM MedicineDonationModel d WHERE d.isActive = '1' AND d.statusId = '1' ORDER BY d.createdAt DESC")
-	List<MedicineDonationModel> findAll();
+	/*
+	 * @Query("SELECT d FROM MedicineDonationModel d WHERE d.isActive = 1 AND d.statusId = 1 ORDER BY d.createdAt DESC"
+	 * ) //List<MedicineDonationModel> customFindAll(Example<S> example, Pageable
+	 * pageable); <S extends MedicineDonationModel> Page<S> customFindAll(Example<S>
+	 * example, Pageable pageable);
+	 */
 
 	List<MedicineDonationModel> findAllByUserId(Long UserId);
 
