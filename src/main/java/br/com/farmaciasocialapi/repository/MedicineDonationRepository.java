883776134ed8efdca4b6f5eb3b98b19165a731e2
David Kalil Braga <dmmd10@hotmail.com>
2020-11-18 21:14:24 -0300

    bugfix

--- src/main/java/br/com/farmaciasocialapi/repository/MedicineDonationRepository.java
+++ src/main/java/br/com/farmaciasocialapi/repository/MedicineDonationRepository.java
@@ -12,17 +12,8 @@ import br.com.farmaciasocialapi.resources.BaseRepository;
 
 public interface MedicineDonationRepository extends BaseRepository<MedicineDonationModel> {
 
-<<<<<<< HEAD
-	/*
-	 * @Query("SELECT d FROM MedicineDonationModel d WHERE d.isActive = 1 AND d.statusId = 1 ORDER BY d.createdAt DESC"
-	 * ) //List<MedicineDonationModel> customFindAll(Example<S> example, Pageable
-	 * pageable); <S extends MedicineDonationModel> Page<S> customFindAll(Example<S>
-	 * example, Pageable pageable);
-	 */
-=======
 	@Query("SELECT d FROM MedicineDonationModel d WHERE d.isActive = 1 AND d.statusId = 1 ORDER BY d.createdAt DESC")
 	List<MedicineDonationModel> findAll();
->>>>>>> 15c3a65cffbfef078391abb55a99f69e5ccc12cd
 
 	List<MedicineDonationModel> findAllByUserId(Long UserId);
 
