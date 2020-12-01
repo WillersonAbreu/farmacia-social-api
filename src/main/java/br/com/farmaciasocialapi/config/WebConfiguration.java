9b8b2bf0cedccf84a61df59e4cc9a00dc2916d7a
WillersonAbreu <willersonabreu@hotmail.com>
2020-12-01 17:09:06 -0300

    Acesso para as imagens

--- src/main/java/br/com/farmaciasocialapi/config/WebConfiguration.java
+++ src/main/java/br/com/farmaciasocialapi/config/WebConfiguration.java
@@ -6,12 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 
 @Configuration
 public class WebConfiguration extends WebMvcConfigurationSupport {
-
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/**")
-		.addResourceLocations("classpath:/","classpath:/images/")
-		.setCachePeriod(0);
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/", "classpath:/images/**")
+				.setCachePeriod(0);
 	}
-	
+
 }
